bfd0cb3a20046bcf1903b5d78c87087548cada41
Michelle Wu <maw035@ucsd.edu>
2015-10-25 20:16:02 -0700

    Added files from previous work session

---
+++ RayTraceable/RayTraceable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTraceable
+//
+//  Created by Michelle Wu on 10/11/15.
+//  Copyright (c) 2015 Michelle Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
